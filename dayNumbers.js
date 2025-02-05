bc013951c1c205f5b45f080c6e76a4fc2be127ff
charite-uwatwembi <c.uwatwembi@alustudent.com>
2025-02-05 21:04:44 +0200

    dayNumbers.js

---
+++ dayNumbers.js
@@ -0,0 +1,33 @@
+
+
+
+
+function getDayNumber(day) {
+    switch (day) {
+        case 1:
+            return "Monday";
+        case 2:
+            return "Tuesday";
+        case 3:
+            return "Wednesday";
+        case 4:
+            return "Thursday";
+        case 5:
+            return "Friday";
+        case 6:
+            return "Saturday";
+        case 7:
+            return "Sunday";
+        default:
+            return "Invalid day number- Enter a number between 1 and 7";
+    }
+}
+
+console.log(getDayNumber(1));
+console.log(getDayNumber(2));
+console.log(getDayNumber(3));
+console.log(getDayNumber(4));
+console.log(getDayNumber(5));
+console.log(getDayNumber(6));
+console.log(getDayNumber(7));
+console.log(getDayNumber(8));
